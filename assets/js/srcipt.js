7042211d27d35200cb0a50fa2e9ecfb5684cb03d
klmitchy <karenapolis@gmail.com>
2022-04-06 11:31:25 -0500

    NYTimes column

--- assets/js/srcipt.js
+++ assets/js/srcipt.js
@@ -77,7 +77,16 @@ function listRecommendations(data) {
 }
 
 
-
+const starTotal = 5;
+ 
+for(const rating in ratings) {  
+  // 2
+  const starPercentage = (ratings[rating] / starTotal) * 100;
+  // 3
+  const starPercentageRounded = `${(Math.round(starPercentage / 10) * 10)}%`;
+  // 4
+  document.querySelector(`.${rating} .stars-inner`).style.width = starPercentageRounded; 
+}
 
 
   // .items = array of 10 books.
