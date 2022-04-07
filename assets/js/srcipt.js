6e2b7bfc59761cae8b6361adb59459f09c7564d1
Ben Powell <powe0186@gmail.com>
2022-04-07 09:53:21 -0500

    took 'horror' out of the url. lol

--- assets/js/srcipt.js
+++ assets/js/srcipt.js
@@ -18,8 +18,7 @@ searchBtn.on('click', function() {
     var bookLink = 'https://www.googleapis.com/books/v1/volumes?q=inauthor:' + searchParam;
     console.log(bookLink);
   } else if (keyOrAuth === 'keyphrase') {
-    var paramArray = searchParam.split(' ');
-    var bookLink = 'https://www.googleapis.com/books/v1/volumes?q=horror' + searchParam;
+    var bookLink = 'https://www.googleapis.com/books/v1/volumes?q=' + searchParam;
 
   }
 
